98bed4a10e26736786bc4da3d715faae78858674
SAMRATHDEEPSINGH <samrathdeepsingh19@gmail.com>
2024-04-07 18:59:18 +0530

    4/7/2024

---
+++ FRONTEND/04_project/minicontext/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from './components/Login';
+import './App.css'
+import Profile from './components/Profile';
+import UserContextProvider from './context/UserContextProvider';
+function App() {
+
+  return (
+   <UserContextProvider>
+    <h1>Hello</h1>
+    <Login/>
+    <Profile/>
+   </UserContextProvider>
+  )
+}
+
+export default App
